9b9dc72c461617751aa03d7528ab5c5e9cd5d93f
Simple-temp <fiveriam01@gmail.com>
2021-10-23 08:14:33 -1100

    javascript

---
+++ index.js
@@ -0,0 +1,86 @@
+// -------------------------------------------------------------Js variable
+
+// var string = "Hello I am Mr js"; //the data type is string and checkout the data type of this coomand = typeof 
+// console.log(typeof(string));
+
+// var number = 2; //the data type is string and checkout the data type of this coomand = typeof 
+// console.log(typeof(number));
+
+// var array = [1,2,3,4]; //the data type is string and checkout the data type of this coomand = typeof 
+// console.log(typeof(array));
+
+// var object ={Name:"A_One", age :"21",Job:"A webdeveloper"}; //the data type is string and checkout the data type of this coomand = typeof 
+// console.log(typeof(object));
+
+
+// ------------------------------------------------------------Js variable some math or test
+
+// var a = 20;
+// var b = 30;
+// var result = a+b;
+// console.log(result);
+
+// var a = 20;
+// var b = 30;
+// var result = a-b;
+// console.log(result);
+
+// var a = 20;
+// var b = 30;
+// var result = a*b;
+// console.log(result);
+
+
+// var a = 20;
+// var b = 30;
+// var result = a/b;
+// console.log(result);
+
+
+// var a = 20;
+// var b = 30;
+// var result = a%b;
+// console.log(result);
+
+
+// -------------------------------------------------------------Js some keyword
+
+
+// var arr = ['hi','hlw','hey','ohh','done','well'];//index will start 0 1 2 3 4....runing
+// // console.log(arr[4]); //index number
+// var letsFind = arr.indexOf(ohh);
+// console.log(letsFind);
+
+// var example = 'I love you';
+// // example.toUpperCase();
+// example.toLowerCase();
+// console.log(example);
+
+
+// -------------------------------------------------------------Js loop
+
+// var num = 0;
+// while(num<=5)
+// {
+//     console.log(num);
+//     num++
+// }
+
+// for (var i = 0 ; i <= 5; i++ )
+// {
+//     console.log(i);
+// }
+
+
+
+
+// -------------------------------------------------------------Js loop in array
+
+// var arr = ['ratul','ridoy','kayes','prottoy'];
+// for(var i = 0 ; i<arr.length ; i++)
+// {
+//     var see = arr[i]
+//     console.log(see);
+// }
+
+console.log ('hello');
